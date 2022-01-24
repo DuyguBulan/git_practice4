db615c18474009dedc4fc03dfcb102c6d65efd27
duygubulan <duygum_ay@hotmail.com>
2022-01-24 10:39:31 -0500

    doing final work on Hello feature

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,7 +3,8 @@ package hello;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
+        System.out.println("B25 learning git and github");
     }
 
 
